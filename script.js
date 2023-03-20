44aa71a44be7cdeb630d4bd366d7967b1a2f0c7e
balagalikhita <balagalikhita@gmail.com>
2023-03-20 22:20:37 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// for car audio- to play infinite times
+var audio=document.createElement('audio');
+audio.setAttribute('src','sound.mp3');
+audio.setAttribute('autoplay','sound.mp3');
+audio.loop=true;
+//audio.play();
